5668c92cb9867303ce2137f9ca7824bd3780ee3b
Xander <Xswiney@umich.edu>
2024-12-12 18:51:43 -0500

    Created workspace

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/xswiney/ece3641-homework6-2024/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/xswiney/ece3641-homework6-2024/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/xswiney/ece3641-homework6-2024/catkin_ws/devel;/opt/ros/melodic' != '' else []
---
+++ catkin_ws/build/xander/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xswiney/ece3641-homework6-2024/catkin_ws/src/xander
+# Build directory: /home/xswiney/ece3641-homework6-2024/catkin_ws/build/xander
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
